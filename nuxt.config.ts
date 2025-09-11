cfd6c6ed5fe842df1770d9b747c0acb2d5f61ef8
kade powell <kadepowell@gmail.com>
2025-09-10 20:48:12 -0400

    make static generated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
+import businesses from './app/data/businesses.json'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui-pro',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxt/image', '@nuxt/scripts'],
+
+  // Enable static site generation
+  ssr: true,
 
   devtools: {
     enabled: true
@@ -15,11 +15,26 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/': { prerender: true },
+    '/directory': { prerender: true },
     '/business/**': { prerender: true },
     '/category/**': { prerender: true }
   },
 
   compatibilityDate: '2025-01-15',
+  nitro: {
+    prerender: {
+      routes: [
+        '/',
+        '/directory',
+        // Generate all business detail pages
+        ...businesses.map((business: any) => `/business/${business.slug}`),
+        // Generate all category pages
+        ...Array.from(
+          new Set(businesses.map((business: any) => business.category))
+        ).map(category => `/category/${category}`)
+      ]
+    }
+  },
 
   eslint: {
     config: {
