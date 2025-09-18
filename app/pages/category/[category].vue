44c9aa98b71b68bacc1f7a6aee9ad3a3cb2adf9c
kade powell <kadepowell@gmail.com>
2025-09-17 20:12:15 -0400

    update directory page

--- app/pages/category/[category].vue
+++ app/pages/category/[category].vue
@@ -1,10 +1,18 @@
 <template>
   <div class="p-6 max-w-4xl mx-auto">
     <h1 class="text-3xl font-bold mb-6 capitalize flex items-center gap-2">
-      <UIcon :name="categoryIcon" class="h-8 w-8" />
-      {{ category }} Directory
+      <UIcon
+        :name="categoryIcon"
+        class="h-8 w-8"
+      />
+      <span v-if="category === 'wellness'">Local wellness</span>
+      <span v-else-if="category === 'services'">Local Services</span>
+      <span v-else>{{ category }} Local</span>
     </h1>
-    <div v-if="filtered.length === 0" class="text-center py-10">
+    <div
+      v-if="filtered.length === 0"
+      class="text-center py-10"
+    >
       No businesses found in this category.
     </div>
     <div class="grid md:grid-cols-2 gap-6">
@@ -14,7 +22,11 @@
         class="p-4 rounded-lg shadow hover:shadow-lg"
       >
         <div class="flex items-center gap-3 mb-2">
-          <UAvatar icon="i-lucide-image" size="3xl" :src="business.logo" />
+          <UAvatar
+            icon="i-lucide-image"
+            size="3xl"
+            :src="business.logo"
+          />
           <div>
             <h2 class="text-lg font-semibold">
               {{ business.name }}
@@ -26,36 +38,37 @@
           variant="ghost"
           :to="`/business/${business.slug}`"
           class="btn mt-2"
-          >View Details →</UButton
         >
+          View Details →
+        </UButton>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from "vue-router";
-import type { Business } from "~/app.vue";
-import businesses from "~/data/businesses.json";
+import { useRoute } from 'vue-router'
+import type { Business } from '~/app.vue'
+import businesses from '~/data/businesses.json'
 
-const route = useRoute();
-const category = route.params.category as string;
+const route = useRoute()
+const category = route.params.category as string
 
 // Get valid categories from the data
 const validCategories = Array.from(
-  new Set((businesses as unknown as Array<Business>).map((b) => b.category)),
-);
+  new Set((businesses as unknown as Array<Business>).map(b => b.category))
+)
 
 // Check if the category is valid
 if (!validCategories.includes(category)) {
   throw createError({
     statusCode: 404,
-    statusMessage: "Category not found",
-  });
+    statusMessage: 'Category not found'
+  })
 }
 
 const filtered = (businesses as unknown as Array<Business>).filter(
-  (b) => b.category === category,
-);
-const categoryIcon = useCategoryIcon(category);
+  b => b.category === category
+)
+const categoryIcon = useCategoryIcon(category)
 </script>
